8daed4d41a630af62230aa6ec011db456925dc21
Ilam Ball <informasiilam@gmail.com>
2024-04-02 15:18:22 +0700

    menambahkan perulangan for

--- activitystructarr.cpp
+++ activitystructarr.cpp
@@ -15,20 +15,21 @@ struct Mahasiswa {
 
 int main (){
     Mahasiswa mhs[3];
-    cout << "Nomor Mahasiswa : ";
-    getline(cin, mhs.nim);
-    cout << "Nama Mahasiswa : ";
-    getline(cin,mhs.nama);
+    for(int i=0;i<3;i++){
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs.nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin,mhs.nama);
 
-    cout << "Alamat Mahasiswa : "<< endl;
-    cout << "\t Nama Desa : ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs.alamat.kota;
-    
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
-
+        cout << "Alamat Mahasiswa : "<< endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs.alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs.alamat.kota;
+        
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs.umur;
+    }
     cout << endl;
     cout << "\n NIM : "<< mhs.nim;
     cout << "\n Nama : "<< mhs.nama;
