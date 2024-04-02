34e7bccb3d86c09134dbf381c1a9c5347a7bc594
Ilam Ball <informasiilam@gmail.com>
2024-04-02 14:40:46 +0700

    membuat hasil struktur detailalamat (desa dan kota)

--- activitystruct2.cpp
+++ activitystruct2.cpp
@@ -20,7 +20,7 @@ int main (){
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
 
-    cout << "Alamat Mahasiswa : ";
+    cout << "Alamat Mahasiswa : "<< endl;
     cout << "\t Nama Desa : ";
     cin >> mhs.alamat.desa;
     cout << "\t Nama Kota : ";
@@ -32,6 +32,8 @@ int main (){
     cout << endl;
     cout << "\n NIM : "<< mhs.nim;
     cout << "\n Nama : "<< mhs.nama;
-    cout << "\n Alamat : "<< mhs.alamat;
+    cout << "\n Alamat : ";
+    cout << "\n Desa : "<< mhs.alamat.desa;
+    cout << "\n Kota : "<< mhs.alamat.kota;
     cout << "\n Umur : "<< mhs.umur;
 }
